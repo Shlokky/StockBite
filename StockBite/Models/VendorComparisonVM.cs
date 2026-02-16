c4bcc73e5f842b00e0febaf43aeb41f38c678fdc
codingsuxks <hgohel@confederationcollege.ca>
2026-02-16 18:01:17 -0500

    new changes in the code

---
+++ StockBite/Models/VendorComparisonVM.cs
@@ -0,0 +1,9 @@
+﻿namespace StockBite.Models
+{
+    public class VendorComparisonVM
+    {
+        public string ProductName { get; set; }
+
+        public List<VendorProduct> VendorOptions { get; set; }
+    }
+}
