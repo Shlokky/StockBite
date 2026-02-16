97e116933b0c83368230de3ec1f0650cbe09190b
codingsuxks <hgohel@confederationcollege.ca>
2026-02-16 16:01:20 -0500

    add Vendor and VendorProduct entities with DB relationship
    Added some new classes and made some changes in the ApplicationDbContext file to add those pages over there

--- StockBite/Data/ApplicationDbContext.cs
+++ StockBite/Data/ApplicationDbContext.cs
@@ -11,6 +11,10 @@ namespace StockBite.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
 
+        public DbSet<Vendor> Vendors { get; set; }
+
+        public DbSet<VendorProduct> VendorProducts { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
